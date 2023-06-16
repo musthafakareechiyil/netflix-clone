75b997997b5aaf0c0b99b6d8d0a0058f73eac44b
musthafa <musthafakareechiyil@gmail.com>
2023-06-16 15:30:47 +0530

    camera menon

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello world Welcome to my youtube channel Musthafa vlogs</h1>
+      <h2>camera manon : Shinas kuttyadi(DON)</h2>
+    </>
+  )
+}
+
+export default App
