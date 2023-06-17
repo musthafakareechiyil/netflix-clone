11c9f5f9a6cc0aae3caf54e28e2d6639eff2b7fe
musthafa <musthafakareechiyil@gmail.com>
2023-06-17 16:34:00 +0530

    v.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 import './App.css'
 import {action,comedy,documetery,horror,originals, romance, trending} from './urls'
 import Navbar from './Components/NavBar/NavBar'
@@ -12,13 +11,13 @@ function App() {
        
       <Navbar />
       <Banner />
-      <RowPost url = {originals} title = "Netflix Originals" />
-      <RowPost url = {action} title = 'Action' isSmall />
       <RowPost url = {trending} title = "Trending" />
-      <RowPost url = {horror} title = 'Horror' isSmall />
+      <RowPost url = {action} title = 'Action' isSmall />
       <RowPost url = {comedy} title = "Comedy" />
-      <RowPost url = {romance} title = 'Romance' isSmall />
+      <RowPost url = {horror} title = 'Horror' isSmall />
       <RowPost url = {documetery} title = "Documentries" />
+      <RowPost url = {romance} title = 'Romance' isSmall />
+
 
     </>
   )
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,28 +1,48 @@
-import React, { useEffect,useState } from 'react'
-import {API_KEY,imageUrl} from '../../constants/constants'
-import axios from '../../axios'
-import "./Banner.css"
+import React, { useEffect, useState } from 'react';
+import { API_KEY, imageUrl } from '../../constants/constants';
+import axios from '../../axios';
+import './Banner.css';
 
 function Banner() {
-  const [movie, setMovie] = useState()
+  const [movies, setMovies] = useState([]);
+  const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+
   useEffect(() => {
-    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      setMovie(response.data.results[0])
-    })
-  }, [])
+    axios
+      .get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMovieIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % movies.length;
+        return nextIndex;
+      });
+    }, 6000);
+
+    return () => {
+      clearInterval(interval); // Clean up the interval when the component unmounts
+    };
+  }, [movies.length]);
+
+  const currentMovie = movies[currentMovieIndex];
+
   return (
-    <div className='banner' style={{ backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ''})` }}>
-      <div className='content'>
-        <h1 className='title'>{movie ? movie.title:''}</h1>
-        <div className='banner-buttons'>
-          <button className='button'>Play</button>
-          <button className='button'>My List</button>
+    <div className="banner" style={{ backgroundImage: `url(${currentMovie ? imageUrl + currentMovie.backdrop_path : ''})` }}>
+      <div className="content">
+        <h1 className="title">{currentMovie ? currentMovie.title : ''}</h1>
+        <div className="banner-buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
         </div>
-        <h1 className='description'>{movie ? movie.overview : ''}</h1>
+        <h1 className="description">{currentMovie ? currentMovie.overview : ''}</h1>
       </div>
-      <div className='fade_bottom'></div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
